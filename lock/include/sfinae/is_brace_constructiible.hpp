f14e4d2994aef90bb7d2ae2bfcfefc0048e7f953
Curve <fynnbwdt@gmail.com>
2021-05-21 20:20:05 +0200

    Merge branch 'master' of github.com:Soundux/lockpp

--- lock/include/sfinae/is_brace_constructiible.hpp
+++ lock/include/sfinae/is_brace_constructiible.hpp
@@ -9,10 +9,10 @@ namespace sxl
     {
         static auto test(...) -> std::uint8_t;
         template <typename clazz, typename... O>
-        static auto test(clazz, O...) -> decltype(clazz{std::declval<O>()...}, std::uint16_t{});
+        static auto test(clazz *, O *...) -> decltype(clazz{std::declval<O>()...}, std::uint16_t{});
 
       public:
         static const bool value =
-            sizeof(test(std::declval<class_type>(), std::declval<T>()...)) == sizeof(std::uint16_t);
+            sizeof(test(std::declval<class_type *>(), std::declval<T *>()...)) == sizeof(std::uint16_t);
     };
 } // namespace sxl
