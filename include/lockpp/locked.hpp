de28b27b271e2075dfcffa947969826e1ecdd404
Curve <fynnbwdt@gmail.com>
2023-10-10 16:36:41 +0200

    fix: formatting

--- include/lockpp/locked.hpp
+++ include/lockpp/locked.hpp
@@ -3,16 +3,19 @@
 
 namespace lockpp
 {
-    template <typename Type, typename Lock> class locked
+    template <typename Type, typename Lock>
+    class locked
     {
-        template <typename, decayed> friend class lock;
+        template <typename, decayed>
+        friend class lock;
 
       private:
         Lock m_lock;
         Type *m_value;
 
       protected:
-        template <typename... LockArgs> locked(Type *, lock_mutex_t<Lock> &, LockArgs &&...);
+        template <typename... LockArgs>
+        locked(Type *, lock_mutex_t<Lock> &, LockArgs &&...);
 
       public:
         [[nodiscard]] Type &operator*() const noexcept;
